e81aa1c1ab0736fe3fa9f81ba08dec9d232f4d22
alessandrobardini <alessandro.bardini93@gmail.com>
2022-03-08 16:48:22 +0100

    add MapBoard

---
+++ app/javascript/components/Overview/MapBoard.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './MapBoard.scss'
+
+const MapBoard: React.FC = () => {
+  return <div className='MapBoard'>
+    This is the map
+  </div>
+}
+
+export default MapBoard
--- app/javascript/pages/Overview/Overview.tsx
+++ app/javascript/pages/Overview/Overview.tsx
@@ -1,13 +1,10 @@
-import React, { useContext, useState } from 'react'
-import axios from 'axios'
-import csrfToken from 'helpers/csrfToken'
-import Button from 'ui/Button'
+import React, { useContext } from 'react'
 import { SessionContext } from 'App'
-import TopBar, { signOut } from '../../components/Layout/TopBar'
+import TopBar from '../../components/Layout/TopBar'
+import YouLost from 'pages/YouLost'
+import MapBoard from 'components/Overview/MapBoard'
 
 import './Overview.scss'
-import Link from 'ui/Link'
-import YouLost from 'pages/YouLost'
 
 const Overview: React.FC = () => {
   const { user, gameEndsAt } = useContext(SessionContext)
@@ -18,12 +15,7 @@ const Overview: React.FC = () => {
 
   return <div className='Overview'>
     <TopBar />
-    {/* <div> this is the progress</div>
-    <div> this is the team name</div>
-    <div> this is the timer</div>
-    <div> this is the logout</div> */}
-    {/* <div>This is the overview page of the game</div>
-    <Button type='danger' onClick={() => signOut().then(() => window.location.replace('/app'))}>Logout</Button> */}
+    <MapBoard />
   </div>
 }
 
