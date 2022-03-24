754b8660cfc49bbceb331a7fcbba26f3522794a7
alessandrobardini <alessandro.bardini93@gmail.com>
2022-03-24 19:52:22 +0100

    Better layout for homepage

--- app/javascript/pages/Home/Home.tsx
+++ app/javascript/pages/Home/Home.tsx
@@ -16,40 +16,61 @@ const Home: React.FC = () => {
   }
 
     return <div className='Home'>
-      <div className='container'>
+      <div className='container left'>
         <h1>Online Company Event</h1>
         <h1>March 2022</h1>
         <p>Powered by the engineering team</p>
         <img src={logo}></img>
-        <p>TODO: Text providing a short introduction to the game...</p>
-
+        <div className='explanation'>
+          <p>Are you ready to enter the kingdom of Magaland?</p>
+          <p>An evil king is reigning mercilessy with the help of his four minions!</p>
+          <p>As a valiant knight, you are asked to defeat them and enter the king's castle!</p>
+          <p>Hurry up, Magaland needs your help!</p>
+        </div>
+        <div className='suggestions'>
+          <p>Rules</p>
+          <ul>
+            <li>You have 60 minutes to clear the game. Once you register, the timer starts!</li>
+          </ul>
+        </div>
+        <div className='suggestions'>
+          <p>The bag</p>
+          <ul>
+            <li>During your adventure, you will find some objects. You can have a look at them in every moment, by clicking on the bag icon on the top left</li>
+            <li>The objects you'll find are vital to solve the riddles! Don't overlook them!</li>
+            <li>Are you playing with some friends? You can share the URL of the bag with them, so that everyone can access its content</li>
+            <li>Remember your credentials: they are needed to access the bag, in case you share the URL</li>
+          </ul>
+        </div>
+      </div>
+      <div className='container'>
         <Form onSubmit={handleSubmit} initialValues={{name: '', password: ''}}>
-        {({ errors, touched, values, setFieldValue }) => (
-            <>
-              <Input
-                label='Team name'
-                formik
-                name='name'
-                errors={errors}
-                touched={touched}
-                value={values.name}
-                setFieldValue={setFieldValue}
-                placeholder='Team name'
-              />
-              <Input
-                label='Password'
-                formik
-                type='password'
-                name='password'
-                errors={errors}
-                touched={touched}
-                value={values.password}
-                setFieldValue={setFieldValue}
-                placeholder='Password'
-              />
-              <Button size='l' className='m-t-md' submit type='primary'>Start the game!</Button>
-            </>
-        )}
+          {({ errors, touched, values, setFieldValue }) => (
+              <>
+                <Input
+                  label='Team name'
+                  formik
+                  name='name'
+                  errors={errors}
+                  touched={touched}
+                  value={values.name}
+                  setFieldValue={setFieldValue}
+                  placeholder='Team name'
+                />
+                <Input
+                  label='Password'
+                  formik
+                  type='password'
+                  name='password'
+                  errors={errors}
+                  touched={touched}
+                  value={values.password}
+                  setFieldValue={setFieldValue}
+                  placeholder='Password'
+                />
+                <Button size='l' className='m-t-md' submit type='primary'>Start the game!</Button>
+              </>
+          )}
         </Form>
       </div>
     </div>
