d5f453f2696e0b25e6d6218270a566ca8cdd25b3
alessandrobardini <alessandro.bardini93@gmail.com>
2024-07-17 15:35:59 +0200

    Adjust home page

--- app/javascript/pages/Home/Home.tsx
+++ app/javascript/pages/Home/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import logo from 'images/magaloop_logo_color.png'
+import logo from 'images/underscape.png'
 import axios from 'axios'
 import csrfToken from 'helpers/csrfToken'
 import Form from 'components/Layout/Form'
@@ -18,15 +18,12 @@ const Home: React.FC = () => {
 
     return <div className='Home'>
       <div className='container left'>
-        <h1>Online Company Event</h1>
-        <h1>March 2022</h1>
-        <p>Powered by the engineering team</p>
         <img src={logo}></img>
         <div className='explanation'>
-          <p>Are you ready to enter the kingdom of Magaland?</p>
+          <p>Are you ready to enter a mysterious kingdom?</p>
           <p>An evil king is reigning mercilessy with the help of his four minions!</p>
           <p>As a valiant knight, you are asked to defeat them and enter the king's castle!</p>
-          <p>Hurry up, Magaland needs your help!</p>
+          <p>Hurry up, the kingdom needs your help!</p>
         </div>
         <div className='suggestions'>
           <p>Rules</p>
@@ -39,8 +36,12 @@ const Home: React.FC = () => {
           <ul>
             <li>During your adventure, you will find some objects. You can have a look at them in every moment, by clicking on the bag icon on the top left</li>
             <li>The objects you'll find are vital to solve the riddles! Don't overlook them!</li>
-            <li>Are you playing with some friends? You can share the URL of the bag with them, so that everyone can access its content</li>
-            <li>Remember your credentials: they are needed to access the bag, in case you share the URL</li>
+          </ul>
+        </div>
+        <div className='suggestions'>
+          <p>Credits</p>
+          <ul>
+            <li>Inspired by the videogame <a href='https://undertale.com/' target='_blank'>Undertale</a>. All the characters images are fan-art found on the web</li>
           </ul>
         </div>
       </div>
